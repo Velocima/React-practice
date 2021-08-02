7c2927e864bb95e633ff5327389a887eca462291
polinamoore <p_gerasimenko@yahoo.com>
2021-08-02 17:43:28 +0100

    Add test setup

---
+++ src/components/Track.test.js
@@ -0,0 +1,13 @@
+import Track from './Track';
+import { render, screen } from '@testing-library/react';
+import { describe } from 'yargs';
+import { expect, test } from '@jest/globals';
+
+describe('Likes Monitor', () => {
+    beforeEach(() => render(<Track/>));
+
+    test('', () => {
+
+        expect().toBe();
+    })
+})
